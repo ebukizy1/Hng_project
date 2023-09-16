218b5dfd0120bf7439028d4b495489d48d41fbbc
Emmanuel Ebuka <62916576+ebukizy1@users.noreply.github.com>
2023-09-16 15:01:39 +0100

    Update PersonsController.java

--- src/main/java/com/ebuka/hngtasktwo/controller/PersonsController.java
+++ src/main/java/com/ebuka/hngtasktwo/controller/PersonsController.java
@@ -19,7 +19,7 @@ public class PersonsController {
     @Autowired
     private PersonServices personServices;
 
-    @PostMapping("/sign-up")
+    @PostMapping("/sign_up")
     public ResponseEntity<CreatePersonResponse> createPersonEndpoint(@RequestBody CreatePersonRequest createPersonRequest){
      CreatePersonResponse personResponse =  personServices.createPerson(createPersonRequest);
     return   ResponseEntity.status(HttpStatus.CREATED).body(personResponse);
